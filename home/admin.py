113ca4c80a2bdf5bef17901d1af7462dad83b8d7
az1mjonovislom77 <azimjonovislomjon77@gmail.com>
2026-03-25 13:39:13 +0500

    done

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from home.models import Home
+from home.models import Home, FloorPlan
+
+
+@admin.register(FloorPlan)
+class FloorPlanAdmin(admin.ModelAdmin):
+    list_display = ['id', 'home']
 
 
 @admin.register(Home)
 class HomeAdmin(admin.ModelAdmin):
     list_display = ['id', 'home_number']
+    inlines = ('floor',)
