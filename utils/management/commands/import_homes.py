e1421d25222885df962f7a91729ba35dbc482491
az1mjonovislom77 <azimjonovislomjon77@gmail.com>
2026-03-26 11:35:36 +0500

    done

---
+++ utils/management/commands/import_homes.py
@@ -0,0 +1,39 @@
+import os
+from django.conf import settings
+import pandas as pd
+from django.core.management.base import BaseCommand
+from home.models import Home
+from utils.models import Blocks, Floors
+
+
+class Command(BaseCommand):
+    help = 'Import homes from Excel file'
+
+    def handle(self, *args, **kwargs):
+        file_path = os.path.join(settings.BASE_DIR, 'data', 'homes.xlsx')
+
+        df = pd.read_excel(file_path)
+
+        block_obj, _ = Blocks.objects.get_or_create(title="A")
+
+        for _, row in df.iterrows():
+            floor_number = int(row['floor'])
+            home_number = int(row['home_number'])
+            area = float(row['area'])
+            rooms = int(row['rooms'])
+            price = float(row['price'])
+
+            floor_obj, _ = Floors.objects.get_or_create(number=floor_number)
+
+            Home.objects.update_or_create(
+                home_number=home_number,
+                defaults={
+                    "blocks": block_obj,
+                    "floor": floor_obj,
+                    "area": area,
+                    "rooms": rooms,
+                    "price_per_sqm": price
+                }
+            )
+
+        self.stdout.write(self.style.SUCCESS('Import tugadi ✅'))
