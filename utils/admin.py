e2ff726de552f6bf4adf61d1d227cc45f6707fba
az1mjonovislom77 <azimjonovislomjon77@gmail.com>
2026-03-30 10:37:41 +0500

    done

--- utils/admin.py
+++ utils/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
+
+from home.models import Home
 from utils.models import Blocks, Floors, Renovation
+
+
+class HomeInline(admin.TabularInline):
+    model = Home
+    extra = 1
 
 
 @admin.register(Blocks)
 class BlocksAdmin(admin.ModelAdmin):
     list_display = ['id', 'title']
+    inlines = [HomeInline]
 
 
 @admin.register(Floors)
