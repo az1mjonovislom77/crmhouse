45f70732c8a23ab151d26d449a9b111f5d79e231
az1mjonovislom77 <azimjonovislomjon77@gmail.com>
2026-04-08 16:59:01 +0500

    done

--- client/api/serializers.py
+++ client/api/serializers.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from rest_framework import serializers
 from booking.models import Booking
 from client.models import Client
@@ -23,6 +24,26 @@ class BookingNestSerializer(serializers.ModelSerializer):
         return obj.home.home_status if obj.home else None
 
 
+class BookingMiniSerializer(serializers.ModelSerializer):
+    cash_payment_percent = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Booking
+        fields = '__all__'
+
+    def get_cash_payment_percent(self, obj):
+        if not obj.home:
+            return None
+        total_price = (((obj.home.area or 0) * (obj.home.price_per_sqm or 0)) +
+                       (obj.home.renovation.price if obj.home.renovation else 0))
+
+        if not total_price:
+            return 0
+
+        percent = (obj.cash_payment / Decimal(total_price)) * Decimal(100)
+        return round(percent, 2)
+
+
 class ClientSerializer(serializers.ModelSerializer):
     booking = BookingNestSerializer(source='bookings', many=True, read_only=True)
     home_status_history = serializers.SerializerMethodField()
@@ -37,10 +58,11 @@ class ClientSerializer(serializers.ModelSerializer):
 
 class ClientNestSerializer(serializers.ModelSerializer):
     home_status_history = serializers.SerializerMethodField()
+    booking = BookingMiniSerializer(read_only=True)
 
     class Meta:
         model = Client
-        fields = ['id', 'home_status_history', 'full_name', 'phone_number', 'passport', 'address']
+        fields = ['id', 'booking', 'home_status_history', 'full_name', 'phone_number', 'passport', 'address']
 
     def get_home_status_history(self, obj):
         return HomeStatusHistorySerializer(obj.status_history.all(), many=True).data
