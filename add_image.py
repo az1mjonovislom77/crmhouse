2edc39ff321a008a747b27230039fb3cc407ebdf
az1mjonovislom77 <azimjonovislomjon77@gmail.com>
2026-06-24 10:11:02 +0500

    done

---
+++ add_image.py
@@ -0,0 +1,19 @@
+import os
+import django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+django.setup()
+
+from django.conf import settings
+from django.core.files import File
+from home.models import FloorPlan, Home
+from projects.models.project_models import Block
+
+img_name = '3-uy 0.png'
+img_path = os.path.join(settings.BASE_DIR, 'images', img_name)
+block = Block.objects.get(title='3-block')
+homes = Home.objects.filter(blocks=block).order_by('home_number')
+master = FloorPlan()
+master.image.save(img_name, File(open(img_path, 'rb')), save=True)
+plans = [FloorPlan(home=h, image=master.image.name) for h in homes]
+FloorPlan.objects.bulk_create(plans)
+print(f'Qoshildi: {len(plans)} ta home ga {img_name}')
