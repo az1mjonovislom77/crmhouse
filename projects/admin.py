f7e54649dda27bdc50eb09a07eef19188c0d6123
az1mjonovislom77 <azimjonovislomjon77@gmail.com>
2026-07-15 16:38:59 +0500

    fixed

--- projects/admin.py
+++ projects/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 
-from projects.models.project_models import Block, Floors, Project, Renovation
+from projects.models.project_models import Block, Floors, Project, Renovation, BlockImage
 from projects.models.showroom_models import SVG, Showroom, ShowroomImage
 
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title']
+    list_display = ["id", "title"]
 
 
 class ShowroomInline(admin.TabularInline):
@@ -14,32 +14,38 @@ class ShowroomInline(admin.TabularInline):
     extra = 1
 
 
+class BlockImageInline(admin.TabularInline):
+    model = BlockImage
+    extra = 1
+
+
 @admin.register(Block)
 class BlockAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title']
+    list_display = ["id", "title"]
+    inlines = [BlockImageInline]
 
 
 @admin.register(Floors)
 class FloorsAdmin(admin.ModelAdmin):
-    list_display = ['id', 'number']
+    list_display = ["id", "number"]
 
 
 @admin.register(Renovation)
 class RenovationAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title']
+    list_display = ["id", "title"]
 
 
 @admin.register(SVG)
 class SVGAdmin(admin.ModelAdmin):
-    list_display = ['id']
+    list_display = ["id"]
 
 
 @admin.register(ShowroomImage)
 class ShowroomImageAdmin(admin.ModelAdmin):
-    list_display = ['id']
+    list_display = ["id"]
     inlines = [ShowroomInline]
 
 
 @admin.register(Showroom)
 class ShowroomAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'block', 'image']
+    list_display = ["id", "title", "block", "image"]
